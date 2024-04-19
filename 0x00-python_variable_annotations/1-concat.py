6343975ef877b6625bfbb13cd51785917ddbf0ad
August-IE <Aeromosele9@gmail.com>
2024-04-19 06:55:32 +0000

    Basic annotations - concat

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+'''A typed-annotated function concat that takes two strings as args
+and returns a concatenated string
+'''
+
+
+def concat(str1: str, str2: str) -> str:
+    '''Concatenates two strings.'''
+    return str1 + str2
